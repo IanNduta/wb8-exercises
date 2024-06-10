a12c29fb07c1fa3daff09bfbb24243c3580bdff4
Ian Nduta <gathonjiandutai@gmail.com>
2024-06-10 13:36:52 -0400

    fixed the html page and corrected the js file

--- todo/scripts/todo.js
+++ todo/scripts/todo.js
@@ -6,11 +6,35 @@ const subitBtn = document.getElementById("subitBtn");
 window.onload = () =>{
     // console.log("hi");
     subitBtn.onclick = onClickedSubitBtn;
+    
 }
 
 function onClickedSubitBtn(){
     console.log("hi");
     console.log("hi");
+    postNewUserData();
+
 }
 
-// function
+function postNewUserData(){
+    let userData = {
+        userId: document.getElementById("userId").value,
+        title: document.getElementById("title").value,
+        completed: document.getElementById("completed").value,
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {"Content-type":
+                    "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+    .catch(err =>{
+        console.log("error");
+    })
+}
