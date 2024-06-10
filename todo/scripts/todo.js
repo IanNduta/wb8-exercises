5543abe8323aa87fadd4eb57c0ab4ae56b1e3f73
Ian Nduta <gathonjiandutai@gmail.com>
2024-06-10 13:14:07 -0400

    add the forms to the html page and edit the js file

---
+++ todo/scripts/todo.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+const subitBtn = document.getElementById("subitBtn");
+
+window.onload = () =>{
+    // console.log("hi");
+    subitBtn.onclick = onClickedSubitBtn;
+}
+
+function onClickedSubitBtn(){
+    console.log("hi");
+    console.log("hi");
+}
+
+// function
